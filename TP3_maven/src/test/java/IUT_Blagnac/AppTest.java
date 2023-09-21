2ec6b43b62d33f77109894a0be704d2fa9c1acfc
YannRouquie <115616612+YannRouquie@users.noreply.github.com>
2023-09-21 09:56:51 +0000

    update

---
+++ TP3_maven/src/test/java/IUT_Blagnac/AppTest.java
@@ -0,0 +1,20 @@
+package IUT_Blagnac;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
